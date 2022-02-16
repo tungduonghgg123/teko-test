8dd8d04fcc4cb8d9c4ce6d3f9f5d60d76026696c
duong vu tung <tungduonghgg123@gmail.com>
2022-02-16 21:36:59 +0700

    feat: add the base table

--- src/App.js
+++ src/App.js
@@ -2,57 +2,47 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { API } from "./services/API";
+import { Table, Tag, Space } from "antd";
 
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Error Description",
+    dataIndex: "errorDescription",
+    key: "errorDescription",
+  },
+  {
+    title: "Product Image",
+    dataIndex: "image",
+    key: "image",
+  },
+  {
+    title: "Product Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "SKU",
+    dataIndex: "sku",
+    key: "sku",
+  },
+  {
+    title: "Color",
+    dataIndex: "color",
+    key: "color",
+  },
+];
 function App() {
   const { data: products } = API.useGetProductsQuery();
   const { data: colors } = API.useGetColorQuery();
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Table dataSource={products} columns={columns} />;
     </div>
   );
 }
